fa49adc1d31f044ac9ef40c6ccb604b190de39ba
panditdhamdhere <dhamdherepandit@gmail.com>
2023-05-29 23:01:45 +0530

    learnt about enums

---
+++ contracts/enum.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+contract ENUMS {
+    enum Cars {Tata, Toyota, Tesla, Tavera}
+
+// Store enum data
+Cars public car1 = Cars.Tata;
+Cars public car2 = Cars.Toyota;
+Cars public car3 = Cars.Tavera;
+Cars public car4 = Cars.Tesla;
+}
+
+// enums helps us to write clean code
