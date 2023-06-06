60e268b9aed2b983c678f5b1407c60a4560b41e8
panditdhamdhere <dhamdherepandit@gmail.com>
2023-06-06 11:27:48 +0530

    payble address added

--- adress.sol
+++ adress.sol
@@ -9,3 +9,10 @@ contract addressAd {
         owner = msg.sender;
     }
 }
+
+contract pay {
+    address payable public owner;
+    constructor () public {
+        owner = payable(msg.sender);
+    }
+}
