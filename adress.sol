6c568f88e249b610e72052a37ad8a8e2cdc48d0e
panditdhamdhere <dhamdherepandit@gmail.com>
2023-06-06 13:44:22 +0530

    all speacial functions in solidity

--- adress.sol
+++ adress.sol
@@ -16,3 +16,49 @@ contract pay {
         owner = payable(msg.sender);
     }
 }
+
+
+contract MyContractConstructor {
+    constructor() {
+        // Constructor logic here
+    }
+}
+
+
+contract MyContract {
+    fallback() external payable {
+        // Fallback logic here
+    }
+}
+
+
+contract Pandit {
+    receive() external payable {
+        // Receive logic here
+    }
+}
+
+
+contract MyHandle {
+    function handleCallback() external {
+        // Handle callback logic here
+    }
+}
+
+contract Pandit {
+    uint public myVariable;
+    
+    function getMyVariable() public view returns (uint) {
+        return myVariable;
+    }
+}
+
+
+
+contract EventSplFunc {
+    event MyEvent(uint indexed id, string message);
+    
+    function triggerEvent(uint _id, string memory _message) public {
+        emit MyEvent(_id, _message);
+    }
+}
