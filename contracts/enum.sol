c0bb357c766bd3b22f0b3c3cad076515412a2286
panditdhamdhere <dhamdherepandit@gmail.com>
2023-10-08 22:59:42 +0530

    soldity working

--- contracts/enum.sol
+++ contracts/enum.sol
@@ -1,22 +1,18 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-pragma solidity 0.8.19;
+contract Contract {
+    enum Game {
+        Cricket,
+        Football,
+        Chess,
+        Tennis
+    }
 
-contract ENUMS {
-    enum Cars {Tata, Toyota, Tesla, Tavera}
-
-// Store enum data
-Cars public car1 = Cars.Tata;
-Cars public car2 = Cars.Toyota;
-Cars public car3 = Cars.Tavera;
-Cars public car4 = Cars.Tesla;
+    Game public food1 = Game.Cricket;
+    Game public food2 = Game.Football;
+    Game public food3 = Game.Chess;
+    Game public food4 = Game.Tennis;
 }
 
-// enums helps us to write clean codeS
-
-
-enum SolidityDev {
-  Pandit,
-  Yash,
-  Larry
-}
+// enum values are stored as unsigned integers. If there are less than 256 values, it will be stored as a uint8. If you have more than 256 values it will use a uint16 and will grow from there as needed (although I'm not sure how many contracts will ever need more than 65536 values in their enum...). The way this data is stored will become more important when we look to parse our smart contracts from the outside world through the ABI.
