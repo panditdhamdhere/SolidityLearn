d61985b8a0b1d386d185bc44a5fac44225f3b0f8
panditdhamdhere <dhamdherepandit@gmail.com>
2023-10-13 14:12:23 +0530

    new con

---
+++ LW3/Dao/CryptoDevsNFT.sol
@@ -0,0 +1,16 @@
+// // SPDX-License-Identifier: MIT
+
+// pragma solidity ^0.8.19;
+
+// // import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+// // ERC721Enumerable
+// contract CryptoDevsNFT   {
+//     constructor() ("CryptoDevs", "CD") {}
+
+//     function mint() public {
+//         _safeMint(msg.sender, totalSupply());
+//     }
+
+
+// }
