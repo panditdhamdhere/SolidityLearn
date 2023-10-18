4067b825ee16a29a176f22769d7ab0f3ae8c0f09
panditdhamdhere <dhamdherepandit@gmail.com>
2023-10-18 23:27:49 +0530

    basic contract

--- solidity new/basic.sol
+++ solidity new/basic.sol
@@ -1,3 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+contract Hello {
+    string public message;
+    address public to;
+
+    constructor() {
+        to = msg.sender;
+    }
+
+    function sayHello() public view returns (string memory) {
+        return message;
+    }
+}
